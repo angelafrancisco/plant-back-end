56e9e4af248193cf0aaeeb54773ae2a93ca5b6c6
Angela Francisco <angelat.francisco@gmail.com>
2022-03-29 09:59:16 -0700

    updated to mongodb atlas

--- db-utils/connect.js
+++ db-utils/connect.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const mongoURI = process.env.MONGO_URI;
 const db = mongoose.connection;
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true },
-    () => console.log('MongoDB connection established:', mongoURI)
+    () => console.log('MongoDB connection established')
 );
 
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
-db.on('connected', () => console.log('mongo connected:', mongoURI));
+db.on('connected', () => console.log('mongo connected'));
 db.on('disconnected', () => console.log('mongo disconnected'));
