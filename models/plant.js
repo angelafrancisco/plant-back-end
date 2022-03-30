9958f31080f60efc1530bd9405576f75802575f1
Angela Francisco <angelat.francisco@gmail.com>
2022-03-30 14:01:07 -0700

    updated plantSchema to include tasks

--- models/plant.js
+++ models/plant.js
@@ -3,17 +3,26 @@ const Schema = mongoose.Schema;
 
 const plantSchema = new Schema({
     name: { type: String }, // nickname, if blank use type
-    type: { type: String, required: true },
-    image: { type: String },
+    type: { type: String, required: true }, // show example in form i.e. succulent, jade, fiddle leaf
+    image: { type: String }, // start with URL
     potSize: { type: Number, required: true },
-    roomName: { type: String }, // plant location room name
-    direction: { type: "N" | "S" | "E" | "W", required: true }, // window location
+    roomName: { type: "Bedroom" | "Dining Room" | "Kitchen" | "Living Room" | "Office" }, // room, dropdown menu
+    direction: { type: "N" | "S" | "E" | "W", required: true }, // window location, dropdown menu
     userNotes: { type: String },
-    waterSched: { type: Number }, // auto generated?
-    petFriendly: { type: Boolean }, // auto generated?
-    maintenance: { type: "High" | "Medium" | "Low" }, // auto generated?
-    light: { type: "Direct" | "Indirect" } // auto generated?
+    // task is not editable by user, plant will only have 1 task at a time
+    task: {
+        completed: { type: Boolean }, // button to complete task
+        waterSchedule: { type: Number } // # of days between watering task based on date added?
+    }
 }, { timestamps: true })
+
+// FUTURE GOALS!!!
+// API to look up plant type
+// Image upload
+// sun: { type: "Full Sun" | "Indirect Sun" | "Shade"} // API generated?
+// petFriendly: { type: Boolean }, // auto generated? based on API toxicity level?
+// maintenance: { type: "High" | "Medium" | "Low" }, // auto generated based on API data?
+// waterSched: { type: Number }, // auto generated, could be based off of drought/humidity resistant?
 
 const Plant = mongoose.model('Plant', plantSchema);
 
