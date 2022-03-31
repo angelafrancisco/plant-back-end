4c892948d4e1eab2f17dc7ea097542771f197fa8
Angela Francisco <angelat.francisco@gmail.com>
2022-03-31 11:52:33 -0700

    debugging CORS error x2

--- app.js
+++ app.js
@@ -17,10 +17,10 @@ const store = new MongoDBStore({
     collection: 'mySessions'
 });
 
-app.use(cors());
 app.options("", cors());
+app.use(cors());
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X - Requested - With, X - CallbackType, Content - Type, Accept");
     res.header("Cache-Control", "no-cache");
